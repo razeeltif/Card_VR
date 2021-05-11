c5c864c251f28150772a2463c17be02688630227
razeeltif <razeltif@gmail.com>
2021-05-11 23:48:10 +0200

    take all my shit

--- Assets/Scripts/ControllersManager.cs
+++ Assets/Scripts/ControllersManager.cs
@@ -6,12 +6,31 @@ using UnityEngine.XR;
 public class ControllersManager : MonoBehaviour
 {
 
+    static ControllersManager instance = null;
+
     List<UnityEngine.XR.InputDevice> listDevices;
+    List<UnityEngine.XR.InputDevice> listDevicesLeft;
+    List<UnityEngine.XR.InputDevice> listDevicesRight;
+
+    InputDeviceCharacteristics leftControllerCharacteristics = InputDeviceCharacteristics.Left;
+    InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right;
+
+    public InputDevice leftHand;
+    public InputDevice rightHand;
 
 
     void Awake()
     {
+
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+
         listDevices = new List<UnityEngine.XR.InputDevice>();
+        listDevicesLeft = new List<UnityEngine.XR.InputDevice>();
+        listDevicesRight = new List<UnityEngine.XR.InputDevice>();
 
         InputDevices.deviceConnected += onDeviceConnected;
         InputDevices.deviceDisconnected += onDeviceDisconnected;
@@ -27,20 +46,38 @@ public class ControllersManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        leftHand.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+        if(primaryButtonValue)
+        {
+            Debug.LogWarning("JE SUIS PRESSEEE");
+        }
     }
 
 
     private void onDeviceConnected(UnityEngine.XR.InputDevice device)
     {
-        UnityEngine.XR.InputDevices.GetDevices(listDevices);
-        logAllDevices();
-
+        refreshDevices();
     }
 
     private void onDeviceDisconnected(UnityEngine.XR.InputDevice device)
     {
+        refreshDevices();
+    }
+
+    private void refreshDevices()
+    {
         UnityEngine.XR.InputDevices.GetDevices(listDevices);
+
+        UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(leftControllerCharacteristics, listDevicesLeft);
+        if(listDevicesLeft.Count > 0)
+        {
+            leftHand = listDevicesLeft[0];
+        }
+        UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, listDevicesRight);
+        if(listDevicesRight.Count > 0)
+        {
+            rightHand = listDevicesRight[0];
+        }
         logAllDevices();
     }
 
--- Assets/Scripts/HandPresence.cs
+++ Assets/Scripts/HandPresence.cs
@@ -6,18 +6,34 @@ using UnityEngine.XR;
 public class HandPresence : MonoBehaviour
 {
 
-        public delegate void test();
-        public static test onConnect;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public GameObject handModelPrefab;
 
+    //public List<GameObject> controllerPrefabs;
+
+    static ControllersManager instance = null;
+
+    List<UnityEngine.XR.InputDevice> listDevices;
 
     private InputDevice targetDevice;
-    List<UnityEngine.XR.InputDevice> inputDevices;
+    private GameObject spawnedHandModel;
+
+
+    void Awake()
+    {
+
+        listDevices = new List<UnityEngine.XR.InputDevice>();
+
+      /*  InputDevices.deviceConnected += onDeviceConnected;
+        InputDevices.deviceDisconnected += onDeviceDisconnected;*/
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
 
-
-        InputDevices.deviceConnected += Action_performed;
+        refreshDevices();
 
     }
 
@@ -25,28 +41,58 @@ public class HandPresence : MonoBehaviour
     void Update()
     {
 
-    UnityEngine.XR.InputDevices.GetDevices(inputDevices);
-    InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Right, inputDevices);
-
-
-
-    targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
-    if(primaryButtonValue)
-    {
-        Debug.Log("ZAZERTGAZGEAZERGZEAR");
     }
 
-     /*   UnityEngine.XR.InputDevices.GetDevices(inputDevices);
 
-        foreach (var device in inputDevices)
+    private void onDeviceConnected(UnityEngine.XR.InputDevice device)
+    {
+        Debug.Log(string.Format("Connected : name '{0}' and role '{1}'", device.name, device.characteristics.ToString()));
+
+
+
+        refreshDevices();
+
+        if(targetDevice.name != device.name)
         {
-            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
-        }*/
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+        }
+
     }
 
-    private void Action_performed(UnityEngine.XR.InputDevice obj)
+    private void onDeviceDisconnected(UnityEngine.XR.InputDevice device)
     {
-        Debug.Log("Select Button is pressed");
+        Debug.Log(string.Format("Disconnected : name '{0}' and role '{1}'", device.name, device.characteristics.ToString()));
+        
+        if(targetDevice.name == device.name)
+        {
+            Destroy(spawnedHandModel);
+        }
+        refreshDevices();
     }
+
+    private void refreshDevices()
+    {
+        UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, listDevices);    
+        if(listDevices.Count > 0)
+        {
+            targetDevice = listDevices[0];
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+        }
+        else{
+            Debug.LogError("No devices found");
+        }
+    }
+
+
+    private void logAllDevices()
+    {
+        foreach (var device in listDevices)
+        {
+            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.characteristics.ToString()));
+        }        
+    }
+
+
+
 
 }
